2e2aa65734ae75a110b436d0aeb9894d48be1c3d
abigt <abigt@gmail.com>
2016-11-23 14:18:17 +0800

    Initial Commit

---
+++ SFSetting/SFSetting.h
@@ -0,0 +1,19 @@
+//
+//  SFSetting.h
+//  SFSetting
+//
+//  Created by 孔祥波 on 23/11/2016.
+//  Copyright © 2016 Kong XiangBo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SFSetting.
+FOUNDATION_EXPORT double SFSettingVersionNumber;
+
+//! Project version string for SFSetting.
+FOUNDATION_EXPORT const unsigned char SFSettingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFSetting/PublicHeader.h>
+
+
